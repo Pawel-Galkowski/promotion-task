5ceaa9d305bd6ffc65e5c12f2f31a4cb7e5c633e
pawel-galkowski <p.galkowski@ik.pl>
2020-07-16 13:29:01 +0200

    Reapir text

--- script.js
+++ script.js
@@ -1,39 +1,25 @@
-var hideElement = elm => {
+var hideElement = (elm) => {
   $(elm).css("display", "none");
 };
 
-$(document).ready(function() {
-  $(".icon-lock").mouseover(function() {
-    $(this)
-      .next()
-      .css("display", "block");
+$(document).ready(function () {
+  $(".icon-lock").mouseover(function () {
+    $(this).next().css("display", "block");
     $(this).attr("src", "./assets/info-hover.svg");
   });
-  $(".icon-lock").mouseout(function() {
-    $(this)
-      .next()
-      .css("display", "none");
+  $(".icon-lock").mouseout(function () {
+    $(this).next().css("display", "none");
     $(this).attr("src", "./assets/info.svg");
   });
-  $(".icon-lock").on("click", function() {
-    if (
-      $(this)
-        .next()
-        .css("display", "block")
-    ) {
-      $(this)
-        .next()
-        .css("display", "none");
+  $(".icon-lock").on("click", function () {
+    if ($(this).next().css("display", "block")) {
+      $(this).next().css("display", "none");
       $(this).attr("src", "./assets/info.svg");
     } else {
-      $(this)
-        .next()
-        .css("display", "block");
+      $(this).next().css("display", "block");
       $(this).attr("src", "./assets/info-hover.svg");
-      setTimeout(function() {
-        $(this)
-          .next()
-          .css("display", "none");
+      setTimeout(function () {
+        $(this).next().css("display", "none");
         $(this).attr("src", "./assets/info.svg");
       }, 5000);
     }
